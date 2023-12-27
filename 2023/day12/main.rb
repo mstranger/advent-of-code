a8d0659477998aa5f0e274bd98799966985f4acd
mstranger <mr.mstranger@gmail.com>
2023-12-27 17:53:57 +0200

    Day 12. Part two

--- 2023/day12/main.rb
+++ 2023/day12/main.rb
@@ -2,7 +2,7 @@
 ### Aoc 2023. Day 12
 ###
 
-# NOTE: it takes about 52s
+# it takes ~53s
 def part_one_answer(file = "input.txt")
   data = File.read(file).split("\n")
 
@@ -14,14 +14,36 @@ def part_one_answer(file = "input.txt")
   end.sum
 end
 
-def part_two_answer
+# it taske ~0.3s
+def part_one_answer_v2(file = "input.txt")
+  data = File.read(file).split("\n")
+  data.sum do |line|
+    pattern, groups = line.split
+    groups = groups.split(",").map(&:to_i)
+
+    find_groups(pattern.chars, groups)
+  end
+end
+
+# it takes ~4s
+def part_two_answer(file = "input.txt")
+  data = File.read(file).split("\n")
+  data.sum do |line|
+    pattern, groups = line.split
+    groups = groups.split(",").map(&:to_i)
+
+    pattern = ([pattern] * 5).join("?")
+    groups *= 5
+
+    find_groups(pattern.chars, groups)
+  end
 end
 
 # ----
 
 def solve(chars, seq)
   indexes = chars.each_with_index.filter_map { |ch, i| i if ch == "?" }
-  (0...2**indexes.length).count do |e|
+  (0...(2**indexes.length)).count do |e|
     # TODO: here
     indexes.each_with_index { |i, j| chars[i] = (e & (1 << j)) == 0 ? "." : "#" }
     arrangement(chars.join) == seq
@@ -32,14 +54,47 @@ def arrangement(line)
   line.scan(/(#+)/).map { _1.first.length }
 end
 
-data = <<~INPUT
-  ???.### 1,1,3
-  .??..??...?##. 1,1,3
-  ?#?#?#?#?#?#?#? 1,3,1,6
-  ????.#...#... 4,1,1
-  ????.######..#####. 1,6,5
-  ?###???????? 3,2,1
-INPUT
+# recursive search
+# recurse down if '#' and count for this group is correct
+# recurse down if '.', breaking current group if applicable
+# explore both paths above if '?'
+def find_groups(pattern, groups, cache = {}, curr = 0)
+  key = [pattern, groups, curr]
+  return cache[key] if cache.key?(key)
+
+  to_grow = (!groups.empty? && groups[0] > curr)
+  valid = (!groups.empty? && groups[0] == curr)
+
+  found = 0
+  if pattern.empty?
+    # end of pattern is end of search
+    # but success means we either got here with the right group count
+    # or we counted all the groups successfully before we got here
+    return ((groups.empty? &&  curr == 0) || (groups.size == 1 && groups[0] == curr)) ? 1 : 0
+  else
+    if ["#", "?"].include?(pattern[0]) && to_grow
+      # always continue counting current group
+      # unless we exhausted our groups
+      found += find_groups(pattern[1..], groups, cache, curr + 1)
+    end
+    if [".", "?"].include?(pattern[0])
+      # continue not counting group
+      if curr == 0
+        found += find_groups(pattern[1..], groups, cache, 0)
+      # break current group if it's correct, otherwise abort path
+      elsif valid
+        found += find_groups(pattern[1..], groups[1..], cache, 0)
+      end
+    end
+  end
+
+  cache[key] = found
+  found
+end
 
 # p part_one_answer
+# p part_one_answer_v2
 # 7163
+
+# p part_two_answer
+# 17_788_038_834_112
