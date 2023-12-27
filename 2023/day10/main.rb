1b0d126d856db854e829f59af01d17425927a3b8
mstranger <mr.mstranger@gmail.com>
2023-12-27 20:17:11 +0200

    Day 10. Part two

--- 2023/day10/main.rb
+++ 2023/day10/main.rb
@@ -3,75 +3,85 @@
 ###
 
 def part_one_answer(file = "input.txt")
-  data = parse(File.read(file))
-  start_point = find_start(data)
-  directions = possible_starts(data, start_point)
-
-  result = directions.map { |start| go_forward(data, start, {}) }.max + 1
-  result / 2
+  data = File.read(file).split("\n")
+  walk(data).size / 2
 end
 
-def part_two_answer
-  raise "Not implemented"
+def part_two_answer(file = "input.txt")
+  data = File.read(file).split("\n")
+  visited = walk(data).to_a
+
+  vertices = []
+  visited.size.times do |i|
+    y, x = visited[i]
+    vertices << [y, x] if %w[F 7 L J].include?(data[y].chars[x])
+  end
+
+  picks_interior(shoelace(vertices), visited.size).to_i
 end
 
-def parse(data)
-  data.split("\n").map(&:chars)
-end
+# ----
 
-def find_start(data)
-  data.each_with_index do |x, i|
-    x.each_with_index do |y, j|
-      return [i, j] if y == "S"
+def walk(data)
+  y = data.find_index { |row| row.index("S") }
+  x = data[y].index("S")
+
+  visited = [[y, x]].to_set
+
+  next_items = []
+
+  # originally started 2 directions at once and counted steps to meet in the middle
+  # but shoelace wants all vertices in order, so change to launche one search node
+  if ["F", "|", "7"].include?(data[y - 1][x])
+    next_items << [[y - 1, x], [-1, 0], 1]
+  elsif ["L", "|", "J"].include?(data[y + 1][x])
+    next_items << [[y + 1, x], [1, 0], 1]
+  elsif ["L", "-", "F"].include?(data[y][x - 1])
+    next_items << [[y, x - 1], [0, -1], 1]
+  elsif ["7", "-", "J"].include?(data[y][x + 1])
+    next_items << [[y, x + 1], [0, 1], 1]
+  end
+
+  until next_items.empty?
+    curr, vec = next_items.shift
+    visited << curr
+    case data[curr[0]][curr[1]]
+    when "-", "|" # vector stays the same
+    when "F", "J" then vec = [-1 * vec[1], -1 * vec[0]]
+    when "L", "7" then vec = [vec[1], vec[0]]
+    end
+
+    unless visited.include?([curr[0] + vec[0], curr[1] + vec[1]])
+      next_items.push([[curr[0] + vec[0], curr[1] + vec[1]], vec])
     end
   end
+
+  visited
 end
 
-# process until the current point is equal to "S" (start)
-def go_forward(data, point, visited)
-  i, j = point
+# https://en.wikipedia.org/wiki/Shoelace_formula
+# area inside a polygon given by the following product/sum of all vertices:
+# A = 1/2*(x0*y1 - x1*y0 + .... x999*y1000 - x1000*y999)
+def shoelace(vertices)
+  sum1 = 0
+  sum2 = 0
 
-  loop do
-    current = data[i][j]
-    return visited.size if current == "S"
-
-    visited["#{i}-#{j}"] = true
-
-    # select next pipe, but not previous
-    next_point = next_variants(current, i, j).select do |x, y|
-      !visited.key?("#{x}-#{y}") && !(data[x][y] == "S" && visited.empty?)
-    end.first
-
-    i, j = next_point
+  vertices.each_with_index do |(x, y), i|
+    next_vertex = vertices[(i + 1) % vertices.size]
+    sum1 += x * next_vertex[1]
+    sum2 += y * next_vertex[0]
   end
+
+  (sum1 - sum2).abs / 2.0
 end
 
-# moves from the start point
-def possible_starts(data, start_point)
-  i, j = start_point
-  right = "-J7".include?(data[i][j + 1])
-  left = "-LF".include?(data[i][j - 1])
-  up = "|F7".include?(data[i - 1][j])
-  down = "|LJ".include?(data[i + 1][j])
-
-  ds = [up, right, down, left]
-  [[i - 1, j], [i, j + 1], [i + 1, j], [i, j - 1]]
-    .select
-    .with_index { |_, idx| ds[idx] == true }
-end
-
-# next move directions
-def next_variants(current, i, j)
-  case current
-  when "|" then [[i + 1, j], [i - 1, j]]
-  when "-" then [[i, j + 1], [i, j - 1]]
-  when "L" then [[i, j + 1], [i - 1, j]]
-  when "J" then [[i, j - 1], [i - 1, j]]
-  when "7" then [[i, j - 1], [i + 1, j]]
-  when "F" then [[i + 1, j], [i, j + 1]]
-  else [[i, j], [i, j]]
-  end
+# gives number of integer points within a simple polygon
+def picks_interior(area, boundary_len)
+  area - (boundary_len / 2.0) + 1
 end
 
 # p part_one_answer
 # 6599
+
+# p part_two_answer
+# 477
--- 2023/day10/main_test.rb
+++ 2023/day10/main_test.rb
@@ -5,7 +5,7 @@ class Day10Test < Minitest::Test
   def setup
     @file = "example.txt"
 
-    data = <<~INPUT
+    @data = <<~INPUT
       ..F7.
       .FJ|.
       SJ.L7
@@ -13,7 +13,7 @@ class Day10Test < Minitest::Test
       LJ...
     INPUT
 
-    File.write(@file, data)
+    File.write(@file, @data)
   end
 
   def teardown
@@ -25,7 +25,20 @@ class Day10Test < Minitest::Test
   end
 
   def test_part_two
-    skip
-    assert_equal 0, part_two_answer(@file)
+    @data = <<~INPUT
+      .F----7F7F7F7F-7....
+      .|F--7||||||||FJ....
+      .||.FJ||||||||L7....
+      FJL7L7LJLJ||LJ.L-7..
+      L--J.L7...LJS7F-7L7.
+      ....F-J..F7FJ|L7L7L7
+      ....L7.F7||L7|.L7L7|
+      .....|FJLJ|FJ|F7|.LJ
+      ....FJL-7.||.||||...
+      ....L---J.LJ.LJLJ...
+    INPUT
+    File.write(@file, @data)
+
+    assert_equal 8, part_two_answer(@file)
   end
 end
