03dbf19358ba52a36ded1e771417343574fa7327
mstranger <mr.mstranger@gmail.com>
2023-12-29 21:32:40 +0200

    Day 23. Part two

--- 2023/day23/main.rb
+++ 2023/day23/main.rb
@@ -31,34 +31,26 @@ def part_one_answer(file = "input.txt")
   end
 
   visited = {}
-  go_forward_v1(map, dirs, target, start, visited, 0)
+  go_forward(map, dirs, target, start, visited, 0)
 end
 
-# TODO: works for test data, but not for the input data
-# stack level too deep
 def part_two_answer(file = "input.txt")
-  map = {}
-  start = nil
-  target = nil
-
   data = File.read(file).split("\n")
-  data.each_with_index do |line, y|
-    line.chars.each_with_index do |ch, x|
-      coord = "#{x},#{y}"
-      start = coord if y == 0 && ch == "."
-      target = coord if y == data.length - 1 && ch == "."
-      map[coord] = ch
-    end
-  end
+  n, m = data.length, data[0].length
+  start = [0, 1]
+  finish = [n - 1, m - 2]
 
-  visited = {}
-  max = { target => 0 }
-  go_forward_v2(map, target, start, visited, 0, max)
+  table = fill_table(data, n, m)
+
+  cache = {}
+  cache[start] = true
+
+  dfs(table, start, finish, 0, cache, 0)
 end
 
 # ----
 
-def go_forward_v1(map, dirs, target, next_step, visited, steps)
+def go_forward(map, dirs, target, next_step, visited, steps)
   x, y = next_step.split(",").map(&:to_i)
 
   visited[next_step] = true
@@ -75,41 +67,69 @@ def go_forward_v1(map, dirs, target, next_step, visited, steps)
   end
 
   lengths = []
-  lengths << go_forward_v1(map, dirs, target, to_visit[0][0], visited.clone, steps + 1) if to_visit[0][1]
-  lengths << go_forward_v1(map, dirs, target, to_visit[1][0], visited.clone, steps + 1) if to_visit[1][1]
-  lengths << go_forward_v1(map, dirs, target, to_visit[2][0], visited.clone, steps + 1) if to_visit[2][1]
-  lengths << go_forward_v1(map, dirs, target, to_visit[3][0], visited.clone, steps + 1) if to_visit[3][1]
+  lengths << go_forward(map, dirs, target, to_visit[0][0], visited.clone, steps + 1) if to_visit[0][1]
+  lengths << go_forward(map, dirs, target, to_visit[1][0], visited.clone, steps + 1) if to_visit[1][1]
+  lengths << go_forward(map, dirs, target, to_visit[2][0], visited.clone, steps + 1) if to_visit[2][1]
+  lengths << go_forward(map, dirs, target, to_visit[3][0], visited.clone, steps + 1) if to_visit[3][1]
   lengths.max || 0
 end
 
-def go_forward_v2(map, target, next_step, visited, steps, max)
-  x, y = next_step.split(",").map(&:to_i)
+# part 2
 
-  visited[next_step] = true
+def neighbours(data, pos)
+  x, y = pos
+  n, m = data.length, data[0].length
 
-  if next_step == target
-    max[target] = steps if steps > max[target]
-    return steps
+  dirs = [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]]
+  dirs.filter do |i, j|
+    i >= 0 && j >= 0 && i < n && j < m && data[i][j] != "#"
+  end
+end
+
+def fill_table(data, n, m)
+  start = [0, 1]
+  finish = [n - 1, m - 2]
+
+  points = [start]
+  (0...n).each do |i|
+    (0...m).each do |j|
+      points << [i, j] if neighbours(data, [i, j]).length > 2
+    end
   end
 
-  to_visit = []
-  [[x - 1, y], [x, y - 1], [x + 1, y], [x, y + 1]].each_with_index do |(x1, y1), i|
-    coord = "#{x1},#{y1}"
-    val = map[coord]
-    can_visit = val && val != "#"
-    can_visit = false unless visited[coord].nil?
-    to_visit[i] = [coord, can_visit]
+  points << finish
+
+  points.each_with_object({}) do |b, g|
+    neighbours(data, b).each do |v|
+      prev, curr = b, v
+      d = 1
+
+      while !points.include?(curr)
+        prev, curr = curr, neighbours(data, curr).filter { |e| e != prev }.first
+        d += 1
+      end
+
+      g.key?(b) ? g[b].push([curr, d]) : g[b] = [[curr, d]]
+    end
+  end
+end
+
+def dfs(map, start, finish, init, visited, max)
+  return [init, max].max if start == finish
+
+  map[start].each do |v, d|
+    next if visited.key?(v)
+
+    visited[v] = true
+    max = [dfs(map, v, finish, init + d, visited, max), max].max
+    visited.delete(v)
   end
 
-  lengths = []
-  lengths << go_forward_v2(map, target, to_visit[0][0], visited.clone, steps + 1, max) if to_visit[0][1]
-  lengths << go_forward_v2(map, target, to_visit[1][0], visited.clone, steps + 1, max) if to_visit[1][1]
-  lengths << go_forward_v2(map, target, to_visit[2][0], visited.clone, steps + 1, max) if to_visit[2][1]
-  lengths << go_forward_v2(map, target, to_visit[3][0], visited.clone, steps + 1, max) if to_visit[3][1]
-  lengths.max || 0
+  max
 end
 
 # p part_one_answer
 # 2074, ~3.5s
 
 # p part_two_answer
+# 6494, ~180s
